c8d2d49a3bf275fdea953ccd24eee9decc00e010
jweezy24 <jackwest245@gmail.com>
2019-06-10 16:43:58 -0500

    file for testing

---
+++ heartbeat.py
@@ -0,0 +1,16 @@
+import os
+import time
+import 
+
+time_low = 0
+time_high = 1
+
+def main():
+    pidfile='/var/run/waggle/heartbeat.pid'
+    cur_PID = os.getpid()
+    f = open(pidfile)
+    for i in f:
+        print(i)
+
+if __name__ == "__main__":
+    main()
