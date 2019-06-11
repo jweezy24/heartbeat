723f335d98dbd72d54494a303424af49d0f5e0ce
jweezy24 <jackwest245@gmail.com>
2019-06-11 08:25:21 -0500

    modularised the code a bit

--- heartbeat.py
+++ heartbeat.py
@@ -9,32 +9,35 @@ time_low = 0
 time_high = 1
 
 
-def do_heartbeat(model):
+def pack_heartbeat(model):
     if "Cx" in model:
         serial = ser.Serial('/dev/ttyS2', 9600, timeout=2)
         messenger = Messenger(serial)
 
         while True:
-            req = pack_sensorgram({
-                'sensor_id': 1,
-                'parameter_id': 0,
-                'value': b'hello',
-                })
-            messenger.write_message(req)
 
-            req = pack_sensorgram({
-                'sensor_id': 2,
-                'parameter_id': 0,
-                'value': b'hello',
-            })
+            sensor_packet(messenger, 0)
 
-            messenger.write_message(req)
+            sensor_packet(messenger, 1)
 
             for resp in messenger.read_messages():
                 print('response')
-                print(unpack_sensorgram(resp))
+                dict = unpack_sensorgram(resp)
+        time.sleep(1)
 
-    time.sleep(1)
+def sensor_packet(messenger, id):
+    if type(num) == type(0):
+        req = pack_sensorgram({
+            'sensor_id': id,
+            'parameter_id': 0,
+            'value': b'hello',
+        })
+
+        messenger.write_message(req)
+    else:
+        print("The id has to be a int")
+        return None
+
 
 
 if __name__ == "__main__":
